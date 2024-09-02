89d3e25401d04e04fc616bd8bba82380e90b12a1
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-02 23:57:20 +0630

    -

--- DotNet8.Packages.GraphQL/Queries/BlogQuery.cs
+++ DotNet8.Packages.GraphQL/Queries/BlogQuery.cs
@@ -4,23 +4,22 @@ using DotNet8.Packages.Extensions;
 using DotNet8.Packages.Shared;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Packages.GraphQL.Queries
-{
-    public class BlogQuery
-    {
-        public async Task<IQueryable<BlogDto>> GetBlogsAsync([Service] AppDbContext context, int pageNo = 1, int pageSize = 10)
-        {
-            try
-            {
-                var query = context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
-                var lst = await query.Paginate(pageNo, pageSize).ToListAsync();
+namespace DotNet8.Packages.GraphQL.Queries;
 
-                return lst.Select(x => x.ToDto()).AsQueryable();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+public class BlogQuery
+{
+    public async Task<IQueryable<BlogDto>> GetBlogsAsync([Service] AppDbContext context, int pageNo = 1, int pageSize = 10)
+    {
+        try
+        {
+            var query = context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+            var lst = await query.Paginate(pageNo, pageSize).ToListAsync();
+
+            return lst.Select(x => x.ToDto()).AsQueryable();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
