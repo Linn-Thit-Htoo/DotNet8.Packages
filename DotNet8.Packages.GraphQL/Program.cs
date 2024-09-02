7335205f0304ea221df9c5de1cfd086f26e8c66a
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-02 11:34:05 +0630

    [ Complete ] Graph QL

--- DotNet8.Packages.GraphQL/Program.cs
+++ DotNet8.Packages.GraphQL/Program.cs
@@ -1,25 +1,20 @@
+using DotNet8.Packages.DbService;
+using DotNet8.Packages.GraphQL.Queries;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQLServer().AddQueryType<BlogQuery>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGraphQL("/");
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
