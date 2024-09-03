86a5a8733f1b71ca4d220822f1442a465f516b4c
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 00:39:18 +0630

    -

--- DotNet8.Packages.AutoMapper/Program.cs
+++ DotNet8.Packages.AutoMapper/Program.cs
@@ -2,11 +2,8 @@ using DotNet8.Packages.AutoMapper.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,7 +11,6 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
