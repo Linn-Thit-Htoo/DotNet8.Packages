3c0fd261be342d53d9c62914a51004fee5acfc3f
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-03 22:10:20 +0630

    [ Added ] AutoMapperProfile

---
+++ DotNet8.Packages.AutoMapper/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DotNet8.Packages.DbService;
+using DotNet8.Packages.DTOs.Blog;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DotNet8.Packages.AutoMapper.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Tbl_Blog, BlogDto>()
+                .ForMember(dest => dest.BlogId,
+                opt => opt.MapFrom(src => src.BlogId))
+                .ForMember(dest => dest.BlogTitle,
+                opt => opt.MapFrom(src => src.BlogTitle))
+                .ForMember(dest => dest.BlogAuthor,
+                opt => opt.MapFrom(src => src.BlogAuthor))
+                .ForMember(dest => dest.BlogContent,
+                opt => opt.MapFrom(src => src.BlogContent));
+        }
+    }
+}
