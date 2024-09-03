cd4dccaf164f7816b03961860ef525da45ee5dd9
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-03 23:26:16 +0630

    [ Complete ] Dumpify

---
+++ DotNet8.Packages.Dumpify/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dumpify;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        new { BlogId = 1, BlogTitle = "Dumpify" }.Dump();
+
+
+        var blog = new BlogModel()
+        {
+            BlogId = 2,
+            BlogTitle = "Blog Title",
+            BlogAuthor = "Blog Author",
+            BlogContent = "Blog Content"
+        }.Dump();
+    }
+}
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
