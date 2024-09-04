38371b583c22f1dd7660f21efe34d66feb3c4e00
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 20:01:12 +0630

    [ Added ] Ftp Service

--- DotNet8.Packages.FluentFtp/Controllers/FtpController.cs
+++ DotNet8.Packages.FluentFtp/Controllers/FtpController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8.Packages.FluentFtp.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.Packages.FluentFtp.Controllers
@@ -7,5 +8,18 @@ namespace DotNet8.Packages.FluentFtp.Controllers
     [ApiController]
     public class FtpController : ControllerBase
     {
+        private readonly FtpService _ftpService;
+
+        public FtpController(FtpService ftpService)
+        {
+            _ftpService = ftpService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckFtpDirectory(string directoryName)
+        {
+            bool isExist = await _ftpService.CheckDirectoryExistsAsync(directoryName);
+            return Ok(isExist);
+        }
     }
 }
--- DotNet8.Packages.FluentFtp/Program.cs
+++ DotNet8.Packages.FluentFtp/Program.cs
@@ -1,3 +1,5 @@
+using DotNet8.Packages.FluentFtp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<FtpService>();
 
 var app = builder.Build();
 
