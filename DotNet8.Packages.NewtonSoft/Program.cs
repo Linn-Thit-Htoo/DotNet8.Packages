3c6c32071d7cd5eb45e8fb8cc8bd1db7c38bcfc2
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 17:40:01 +0630

    [ Added ] Newtonsoft

---
+++ DotNet8.Packages.NewtonSoft/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        #region C# object to JSON
+
+        var blog = new BlogModel()
+        {
+            BlogId = 1,
+            BlogTitle = "Sample Title",
+            BlogAuthor = "Sample Author",
+            BlogContent = "Sample Content"
+        };
+        string jsonStr = blog.ToJson();
+        Console.WriteLine(jsonStr);
+
+        #endregion
+
+        #region JSON to C# object
+
+        var obj = jsonStr.ToObject<BlogModel>();
+        Console.WriteLine(obj.BlogId);
+
+        #endregion
+    }
+}
+
+public static class DevCode
+{
+    public static string ToJson(this object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+
+    public static T ToObject<T>(this string jsonStr)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonStr)!;
+    }
+}
+
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
