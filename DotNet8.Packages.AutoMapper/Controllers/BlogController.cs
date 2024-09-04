fe34af09c3db3241e64425720baf414d11f9f4ea
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-05 00:19:02 +0630

    -

--- DotNet8.Packages.AutoMapper/Controllers/BlogController.cs
+++ DotNet8.Packages.AutoMapper/Controllers/BlogController.cs
@@ -2,28 +2,27 @@
 using DotNet8.Packages.DTOs.Blog;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Packages.AutoMapper.Controllers
+namespace DotNet8.Packages.AutoMapper.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly IMapper _mapper;
+
+    public BlogController(IMapper mapper)
     {
-        private readonly IMapper _mapper;
+        _mapper = mapper;
+    }
 
-        public BlogController(IMapper mapper)
-        {
-            _mapper = mapper;
-        }
+    [HttpPost]
+    public IActionResult CreateBlog([FromBody] BlogRequestDto blogRequest)
+    {
+        return Ok(ToDto(blogRequest));
+    }
 
-        [HttpPost]
-        public IActionResult CreateBlog([FromBody] BlogRequestDto blogRequest)
-        {
-            return Ok(ToDto(blogRequest));
-        }
-
-        private BlogDto ToDto(BlogRequestDto blogRequest)
-        {
-            return _mapper.Map<BlogDto>(blogRequest);
-        }
+    private BlogDto ToDto(BlogRequestDto blogRequest)
+    {
+        return _mapper.Map<BlogDto>(blogRequest);
     }
 }
