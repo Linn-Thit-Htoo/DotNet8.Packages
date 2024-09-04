bd7049ef7329d82dc4609b95483ab5f270621d7d
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 20:09:14 +0630

    [ Added ] BlogModel

---
+++ DotNet8.Packages.Sieve/Models/BlogModel.cs
@@ -0,0 +1,19 @@
+﻿using Sieve.Attributes;
+
+namespace DotNet8.Packages.Sieve.Models
+{
+    public class BlogModel
+    {
+        [Sieve(CanSort = true)]
+        public int BlogId { get; set; }
+
+        [Sieve(CanFilter = true)]
+        public string BlogTitle { get; set; }
+
+        [Sieve(CanFilter = true)]
+        public string BlogAuthor { get; set; }
+
+        [Sieve(CanFilter = true)]
+        public string BlogContent { get; set; }
+    }
+}
