7de0cb2e0ff8164b126b19b500a5a89fa527f230
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 17:40:27 +0630

    -

--- DotNet8.Packages.NewtonSoft/Program.cs
+++ DotNet8.Packages.NewtonSoft/Program.cs
@@ -1,52 +1,51 @@
 ﻿using Newtonsoft.Json;
 
-namespace DotNet8.Packages.NewtonSoft
+namespace DotNet8.Packages.NewtonSoft;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        #region C# object to JSON
+
+        var blog = new BlogModel()
         {
-            #region C# object to JSON
+            BlogId = 1,
+            BlogTitle = "Sample Title",
+            BlogAuthor = "Sample Author",
+            BlogContent = "Sample Content"
+        };
+        string jsonStr = blog.ToJson();
+        Console.WriteLine(jsonStr);
 
-            var blog = new BlogModel()
-            {
-                BlogId = 1,
-                BlogTitle = "Sample Title",
-                BlogAuthor = "Sample Author",
-                BlogContent = "Sample Content"
-            };
-            string jsonStr = blog.ToJson();
-            Console.WriteLine(jsonStr);
+        #endregion
 
-            #endregion
+        #region JSON to C# object
 
-            #region JSON to C# object
+        var obj = jsonStr.ToObject<BlogModel>();
+        Console.WriteLine(obj.BlogId);
 
-            var obj = jsonStr.ToObject<BlogModel>();
-            Console.WriteLine(obj.BlogId);
+        #endregion
+    }
+}
 
-            #endregion
-        }
+public static class DevCode
+{
+    public static string ToJson(this object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
     }
 
-    public static class DevCode
+    public static T ToObject<T>(this string jsonStr)
     {
-        public static string ToJson(this object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
-
-        public static T ToObject<T>(this string jsonStr)
-        {
-            return JsonConvert.DeserializeObject<T>(jsonStr)!;
-        }
+        return JsonConvert.DeserializeObject<T>(jsonStr)!;
     }
+}
 
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
