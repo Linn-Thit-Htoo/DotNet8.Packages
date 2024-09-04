ec2667af34604d499a1a5f41aa2d0fc52ba35123
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 21:39:52 +0630

    [ In Progress ] Drink to pdf

---
+++ DotNet8.Packages.DrinkToPdf/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8.Packages.DrinkToPdf.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserRole { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
