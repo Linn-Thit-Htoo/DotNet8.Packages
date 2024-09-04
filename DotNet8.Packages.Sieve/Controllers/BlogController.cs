014cf667966ede791b7ddf44747dcce48b30e3d9
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 20:33:56 +0630

    [ Added ] Sieve

---
+++ DotNet8.Packages.Sieve/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using DotNet8.Packages.Sieve.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace DotNet8.Packages.Sieve.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly SieveProcessor _sieveProcessor;
+
+        public BlogController(AppDbContext context, SieveProcessor sieveProcessor)
+        {
+            _context = context;
+            _sieveProcessor = sieveProcessor;
+        }
+
+        // https://localhost:7277/api/Blog?Filters=BlogTitle&Sorts=BlogId&Page=1&PageSize=2
+        // https://localhost:7277/api/Blog?Filters=BlogTitle%40%3DBlog%20Title%201
+        // %40 => @
+        // %3D => =
+        [HttpGet]
+        public IActionResult FilterBlogs([FromQuery] SieveModel model)
+        {
+            var blogs = _context.Blogs.AsQueryable();
+            blogs = _sieveProcessor.Apply(model, blogs);
+
+            return Ok(blogs);
+        }
+    }
+}
--- DotNet8.Packages.Sieve/Db/AppDbContext.cs
+++ DotNet8.Packages.Sieve/Db/AppDbContext.cs
@@ -7,11 +7,11 @@ namespace DotNet8.Packages.Sieve.Db
         public List<BlogModel> Blogs { get; } = new()
         {
             new(1, "Blog Title 1", "Blog Author 1", "Blog Content 1"),
-            new(1, "Blog Title 2", "Blog Author 2", "Blog Content 2"),
-            new(1, "Blog Title 3", "Blog Author 3", "Blog Content 3"),
-            new(1, "Blog Title 4", "Blog Author 4", "Blog Content 4"),
-            new(1, "Blog Title 5", "Blog Author 5", "Blog Content 5"),
-            new(1, "Blog Title 6", "Blog Author 6", "Blog Content 6")
+            new(2, "Blog Title 2", "Blog Author 2", "Blog Content 2"),
+            new(3, "Blog Title 3", "Blog Author 3", "Blog Content 3"),
+            new(4, "Blog Title 4", "Blog Author 4", "Blog Content 4"),
+            new(5, "Blog Title 5", "Blog Author 5", "Blog Content 5"),
+            new(6, "Blog Title 6", "Blog Author 6", "Blog Content 6")
         };
     }
 }
