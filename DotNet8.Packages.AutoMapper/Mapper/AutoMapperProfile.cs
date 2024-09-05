92314584dfbaad26673259c2215885b1fdba3e8a
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:24:01 +0630

    -

--- DotNet8.Packages.AutoMapper/Mapper/AutoMapperProfile.cs
+++ DotNet8.Packages.AutoMapper/Mapper/AutoMapperProfile.cs
@@ -1,21 +1,12 @@
-﻿using AutoMapper;
-using DotNet8.Packages.DbService;
-using DotNet8.Packages.DTOs.Blog;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿namespace DotNet8.Packages.AutoMapper.Mapper;
 
-namespace DotNet8.Packages.AutoMapper.Mapper
+public class AutoMapperProfile : Profile
 {
-    public class AutoMapperProfile : Profile
+    public AutoMapperProfile()
     {
-        public AutoMapperProfile()
-        {
-            CreateMap<BlogRequestDto, BlogDto>()
-                .ForMember(dest => dest.BlogTitle,
-                opt => opt.MapFrom(src => src.BlogTitle))
-                .ForMember(dest => dest.BlogAuthor,
-                opt => opt.MapFrom(src => src.BlogAuthor))
-                .ForMember(dest => dest.BlogContent,
-                opt => opt.MapFrom(src => src.BlogContent));
-        }
+        CreateMap<BlogRequestDto, BlogDto>()
+            .ForMember(dest => dest.BlogTitle, opt => opt.MapFrom(src => src.BlogTitle))
+            .ForMember(dest => dest.BlogAuthor, opt => opt.MapFrom(src => src.BlogAuthor))
+            .ForMember(dest => dest.BlogContent, opt => opt.MapFrom(src => src.BlogContent));
     }
 }
