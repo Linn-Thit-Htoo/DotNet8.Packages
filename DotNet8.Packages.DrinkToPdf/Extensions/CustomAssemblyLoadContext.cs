3a0db6f639fa94783fed92beeaadaa027541ff57
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:26:08 +0630

    -

--- DotNet8.Packages.DrinkToPdf/Extensions/CustomAssemblyLoadContext.cs
+++ DotNet8.Packages.DrinkToPdf/Extensions/CustomAssemblyLoadContext.cs
@@ -1,23 +1,19 @@
-﻿using System.Reflection;
-using System.Runtime.Loader;
+﻿namespace DotNet8.Packages.DrinkToPdf.Extensions;
 
-namespace DotNet8.Packages.DrinkToPdf.Extensions
+public class CustomAssemblyLoadContext : AssemblyLoadContext
 {
-    public class CustomAssemblyLoadContext : AssemblyLoadContext
+    public IntPtr LoadUnmanagedLibrary(string absolutePath)
     {
-        public IntPtr LoadUnmanagedLibrary(string absolutePath)
-        {
-            return LoadUnmanagedDll(absolutePath);
-        }
+        return LoadUnmanagedDll(absolutePath);
+    }
 
-        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
-        {
-            return LoadUnmanagedDllFromPath(unmanagedDllName);
-        }
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+    {
+        return LoadUnmanagedDllFromPath(unmanagedDllName);
+    }
 
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            throw new NotImplementedException();
-        }
+    protected override Assembly Load(AssemblyName assemblyName)
+    {
+        throw new NotImplementedException();
     }
 }
--- DotNet8.Packages.DrinkToPdf/GlobalUsings.cs
+++ DotNet8.Packages.DrinkToPdf/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using DotNet8.Packages.DrinkToPdf.Models;
 global using DotNet8.Packages.DrinkToPdf.Services;
 global using Microsoft.AspNetCore.Mvc;
+global using System.Reflection;
+global using System.Runtime.Loader;
