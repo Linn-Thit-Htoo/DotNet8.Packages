d23434d5048bed59009087a401c90255e1aca3cf
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-05 23:56:49 +0630

    [ In Progress ] OData

---
+++ DotNet8.Packages.OData/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.Packages.OData.AppDbContextModels
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
+    }
+}
---
+++ DotNet8.Packages.OData/Program.cs
@@ -0,0 +1,65 @@
+using DotNet8.Packages.OData.AppDbContextModels;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services.AddControllers()
+    .AddOData(options =>
+        options
+            .AddRouteComponents("odata", GetEdmModel())
+            .Select()
+            .Expand()
+            .Filter()
+            .OrderBy()
+            .Count()
+            .SetMaxTop(100)
+            .SkipToken()
+    )
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(
+    opt =>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+    },
+    ServiceLifetime.Transient, ServiceLifetime.Transient
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+static IEdmModel GetEdmModel()
+{
+    var odataBuilder = new ODataConventionModelBuilder();
+    odataBuilder.EntitySet<Tbl_Blog>("Blogs");
+
+    return odataBuilder.GetEdmModel();
+}
