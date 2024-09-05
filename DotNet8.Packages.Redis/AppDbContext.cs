b1648a3d8c08fe5f4f36ab4bef707f2c78d186a5
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-05 23:27:13 +0630

    [ In Progress ] Redis

---
+++ DotNet8.Packages.Redis/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Packages.Redis
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+            {
+                DataSource = ".",
+                InitialCatalog = "testDb",
+                UserID = "sa",
+                Password = "sasa@123",
+                TrustServerCertificate = true
+            };
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ DotNet8.Packages.Redis/BlogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Packages.Redis
+{
+    [Table("Tbl_Blog")]
+    public class BlogModel
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
