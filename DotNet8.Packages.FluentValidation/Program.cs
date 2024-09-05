e180b202ea6be9134d0547e248f27379ebc8aea6
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:32:35 +0630

    -

--- DotNet8.Packages.FluentValidation/Program.cs
+++ DotNet8.Packages.FluentValidation/Program.cs
@@ -1,11 +1,6 @@
-using DotNet8.Packages.FluentValidation.Validators;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +8,6 @@ builder.Services.AddScoped<BlogValidator>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
