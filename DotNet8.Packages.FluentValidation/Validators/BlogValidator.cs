837224e553a4f7d7d35e5dc3f32234ee4e555db8
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:32:08 +0630

    -

--- DotNet8.Packages.FluentValidation/Validators/BlogValidator.cs
+++ DotNet8.Packages.FluentValidation/Validators/BlogValidator.cs
@@ -6,13 +6,22 @@ public class BlogValidator : AbstractValidator<BlogRequestDto>
 {
     public BlogValidator()
     {
-        RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog Title cannot be empty.")
-            .NotNull().WithMessage("Blog Title cannot be null.");
+        RuleFor(x => x.BlogTitle)
+            .NotEmpty()
+            .WithMessage("Blog Title cannot be empty.")
+            .NotNull()
+            .WithMessage("Blog Title cannot be null.");
 
-        RuleFor(x => x.BlogAuthor).NotEmpty().WithMessage("Blog Author cannot be empty.")
-            .NotNull().WithMessage("Blog Author cannot be null.");
+        RuleFor(x => x.BlogAuthor)
+            .NotEmpty()
+            .WithMessage("Blog Author cannot be empty.")
+            .NotNull()
+            .WithMessage("Blog Author cannot be null.");
 
-        RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog Content cannot be empty.")
-            .NotNull().WithMessage("Blog Content cannot be null.");
+        RuleFor(x => x.BlogContent)
+            .NotEmpty()
+            .WithMessage("Blog Content cannot be empty.")
+            .NotNull()
+            .WithMessage("Blog Content cannot be null.");
     }
 }
