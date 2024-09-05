92c3ab48c647982edbc938a323e7c9dcb1efc947
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:27:25 +0630

    -

--- DotNet8.Packages.DrinkToPdf/Program.cs
+++ DotNet8.Packages.DrinkToPdf/Program.cs
@@ -1,14 +1,9 @@
-using DinkToPdf.Contracts;
-using DinkToPdf;
 using DotNet8.Packages.DrinkToPdf.Extensions;
-using DotNet8.Packages.DrinkToPdf.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
