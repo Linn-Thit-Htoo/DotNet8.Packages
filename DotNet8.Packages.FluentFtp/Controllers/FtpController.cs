209e93d0417e1c755dac1452d83173cde4a9dc51
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 00:29:13 +0630

    -

--- DotNet8.Packages.FluentFtp/Controllers/FtpController.cs
+++ DotNet8.Packages.FluentFtp/Controllers/FtpController.cs
@@ -1,25 +1,23 @@
 ﻿using DotNet8.Packages.FluentFtp.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Packages.FluentFtp.Controllers
+namespace DotNet8.Packages.FluentFtp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FtpController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FtpController : ControllerBase
+    private readonly FtpService _ftpService;
+
+    public FtpController(FtpService ftpService)
     {
-        private readonly FtpService _ftpService;
+        _ftpService = ftpService;
+    }
 
-        public FtpController(FtpService ftpService)
-        {
-            _ftpService = ftpService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> CheckFtpDirectory(string directoryName)
-        {
-            bool isExist = await _ftpService.CheckDirectoryExistsAsync(directoryName);
-            return Ok(isExist);
-        }
+    [HttpGet]
+    public async Task<IActionResult> CheckFtpDirectory(string directoryName)
+    {
+        bool isExist = await _ftpService.CheckDirectoryExistsAsync(directoryName);
+        return Ok(isExist);
     }
 }
